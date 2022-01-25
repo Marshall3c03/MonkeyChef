a9725198cc3df91872b09068e4a977de7f1dbfec
Ross Napier <ross-napier@hotmail.com>
2022-01-25 17:05:49 +0000

    Full recipe appears in browser when clicked

--- client/src/containers/RecipeDetail.js
+++ client/src/containers/RecipeDetail.js
@@ -5,12 +5,18 @@ const Recipe = ({recipe, onRecipeClick})=>{
 
     const handleClick = function() {
         onRecipeClick(recipe);
-    }
+    };
+
+    // const getIngredients = recipe.ingredients.map(ingredient => {
+    //         return(ingredient.name);
+    // };
     
     return(
         <>
         <div onClick = {handleClick}>
         <p>{recipe.name}</p>
+        {/* <p>{getIngredients}</p> */}
+        <p>{recipe.method}</p>
         <p></p>
         </div>
         </>
