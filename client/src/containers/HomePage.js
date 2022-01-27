d39dff5758e516b6f1f070b72d81e67b5c4bf621
Craig Marshall <dev.craigmarshall@gmail.com>
2022-01-27 11:31:28 +0000

    Main link buttons added and css added

--- client/src/containers/HomePage.js
+++ client/src/containers/HomePage.js
@@ -1,6 +1,8 @@
 import react from "react";
 import { useNavigate } from "react-router-dom";
 
+import '../static/CSS/homepage.css';
+
 const HomePage = ()=>{
     const navigate = useNavigate();
 
@@ -10,21 +12,31 @@ const HomePage = ()=>{
 
     return(
         <>
-            <div onClick={() => handleClick("/")}>
-                <h1>Home Page</h1>
+        <div onClick={() => handleClick("/")}>
+            <h1>Home Page</h1>
+        </div>
+
+        <div className="link-container">
+            <div className="button-group" onClick={() => handleClick("/addrecipe")}>
+                <p className="button-title" >Add Recipe</p>
+                <img className="button-image" src="https://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/sign-add-icon.png" width="100px" height="100px"/>
             </div>
-            <div onClick={() => handleClick("/addrecipe")}>
-                <p>Add Recipe</p>
+
+            <div className="button-group" onClick={() => handleClick("/recipebook")}>
+                <p className="button-title" >Recipe Book</p>
+                <img className="button-image"src="https://upload.wikimedia.org/wikipedia/commons/3/3a/Book-icon-bible.png" width="100px" height="100px"/>
             </div>
-            <div onClick={() => handleClick("/recipebook")}>
-                <p>Recipe Book</p>
+
+            <div className="button-group" onClick={() => handleClick("/planner")}>
+                <p className="button-title" >Weekly Planner</p>
+                <img className="button-image" src="https://i.pinimg.com/originals/d2/28/d0/d228d012b5f3852abb2b66d9da526801.png" width="100px" height="100px"/>
             </div>
-            <div onClick={() => handleClick("/planner")}>
-            <p>Weekly Planner</p>
+
+            <div className="button-group" onClick={() => handleClick("/shoppingList")}>
+                <p className="button-title" >Shopping List</p>
+                <img className="button-image" src="https://cdn.iconscout.com/icon/free/png-256/shopping-list-2923133-2434627.png" width="100px" height="100px"/>
             </div>
-            <div onClick={() => handleClick("/shoppingList")}>
-                <p>Shopping List</p>
-            </div>
+        </div>
         </>
     );
 };
